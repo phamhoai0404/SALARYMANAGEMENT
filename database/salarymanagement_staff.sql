2652091ae1de9b56495b5d8c580c8c46375049ea
Pham Hoai <hoai04042000@gmail.com>
2021-08-12 17:59:02 +0700

    salarymanagement - 1

---
+++ database/salarymanagement_staff.sql
@@ -0,0 +1,17 @@
+INSERT INTO `staff` (`address`, `basic_salary`, `birthday`, `full_name`, `gender`,`position`, `staff_describe`, `staff_id`) VALUES 
+	('Hải Dương', '7500000', '2000-09-08', 'Phạm Ngọc', 'Nữ','Fresher', 'Tốt nghiệp đại học', 'NV1'),
+    ('Hải Phòng', '7000000', '2000-05-04', 'Lê Nam', 'Nữ','Fresher', '', 'NV2'),
+    ('Hà Nội', '80000000', '1975-05-14', 'Đào Tuấn', 'Nam','Management Director', '', 'NV3'),
+    ('Bắc Giang', '8000000', '1999-10-04', 'Tuấn Nam', 'Nữ','Fresher', 'Tốt nghiệp đại học', 'NV4'),
+    ('Hải Dương', '7500000', '2000-09-08', 'Thu Hà', 'Nữ','Fresher', 'Tốt nghiệp đại học', 'NV5'),
+    ('Hải Phòng', '7000000', '2000-05-04', 'Lê Nam', 'Nữ','Fresher', '', 'NV6'),
+    ('Hà Nội', '8000000', '1995-05-14', 'Đỗ Vinh', 'Nam','Leader Java', '', 'NV7'),
+    ('Bắc Giang', '3000000', '2000-10-04', 'Tuấn Nam', 'Nam','Intership', 'Tốt nghiệp đại học', 'NV8'),
+    ('Hải Dương', '10000000', '2000-09-08', 'Minh Châu', 'Nữ','Senior', 'Tốt nghiệp đại học', 'NV9'),
+    ('Hải Phòng', '7000000', '2000-05-04', 'Lê Thúy', 'Nữ','Fresher', '', 'NV10'),
+    ('TT Huế', '100000000', '1975-05-14', 'Mai Toản', 'Nam','Leader PHP', '', 'NV11'),
+    ('Hà Nam', '8000000', '1999-10-04', 'Nguyễn Thư', 'Nữ','Fresher', 'Tốt nghiệp đại học', 'NV12')
+    
+
+
+    
